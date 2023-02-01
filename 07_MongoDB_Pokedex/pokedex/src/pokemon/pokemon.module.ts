7a357805b20d7cd62dd15a9a79591f4eb61ceb95
carlos-paezf <cpaezferreira@gmail.com>
2023-02-01 12:42:40 -0500

    Add Crear esquemas y modelos

--- 07_MongoDB_Pokedex/pokedex/src/pokemon/pokemon.module.ts
+++ 07_MongoDB_Pokedex/pokedex/src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
-import { PokemonController } from './pokemon.controller';
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity'
+import { PokemonController } from './pokemon.controller'
+import { PokemonService } from './pokemon.service'
 
-@Module({
-  controllers: [PokemonController],
-  providers: [PokemonService]
-})
-export class PokemonModule {}
+@Module( {
+    controllers: [ PokemonController ],
+    providers: [ PokemonService ],
+    imports: [
+        MongooseModule.forFeature( [
+            {
+                name: Pokemon.name,
+                schema: PokemonSchema
+            }
+        ] )
+    ]
+} )
+export class PokemonModule { }
